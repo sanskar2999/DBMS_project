f2e8c37c9294c93941dd8ecd83dde53dbf612749
sanskar <sanskar2999@gmail.com>
2020-12-16 16:42:50 +0530

    temaplates

--- src/views/generate/GenerateView/index.js
+++ src/views/generate/GenerateView/index.js
@@ -1,4 +1,4 @@
-import React, { useState , useEffect} from 'react';
+import React, { Component } from 'react';
 import {
   Box,
   Container,
@@ -12,13 +12,12 @@ import {
   Typography,
   CardHeader
 } from '@material-ui/core';
-import { Pagination } from '@material-ui/lab';
 import Page from 'src/components/Page';
-import Toolbar from './Toolbar';
-import PhotoCamera from '@material-ui/icons/PhotoCamera';
-import * as Yup from 'yup';
 import { Formik } from 'formik';
 import jwt_decode from "jwt-decode";
+import ImagePicker from 'react-image-picker'
+import 'react-image-picker/dist/index.css'
+
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -44,150 +43,192 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-var templates = [];
 var admin_emailId;
 var decoded;
+var file;
+var template;
 
-const ProductList = () => {
-  
-  const classes = useStyles();
-  var file;
-  var template;
-  const [signature, setMember] = React.useState('');
 
-  const handlechange = (event) => {
-    setMember(event.target.value);
-  };
-
-  function _handleImageChange(e) {
-    e.preventDefault();
-
-    let reader = new FileReader();
-    let image = e.target.files[0];
-
-    reader.onloadend = () => {
-      file = image;
+export default class ProductList extends Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      signature: "",
+      image: null,
+      imageList: [],
+      loaded:false
     }
-
-    reader.readAsDataURL(image)
+    this.onPick = this.onPick.bind(this);
+    this.handlechange=this.handlechange.bind(this);
   }
+    componentDidMount(){
+            var templates=[];
+          fetch('http://localhost:5000/templates/')
+          .then(resp => resp.json())
+          .then(data => data.map((images)=>{
+              var imageUrl='http://localhost:5000/'+images.url;
+              templates.push(imageUrl)
+          }))
+          this.setState({
+            imageList: templates,
+            loaded:true,
+          })
+          // console.log(imageList)
+        }
 
-  function _handleTemplateChange(e) {
-    e.preventDefault();
+           handlechange = (event) => {
+            this.setState({
+              signature: event.target.value,
+            })
+          };
 
-    let reader = new FileReader();
-    let image = e.target.files[0];
+           _handleImageChange(e) {
+            e.preventDefault();
 
-    reader.onloadend = () => {
-      template = image;
-    }
+            let reader = new FileReader();
+            let image = e.target.files[0];
 
-    reader.readAsDataURL(image)
-  }
-  
-  return (
-    <Page
-      className={classes.root}
-    >
-      <Container maxWidth="sm">
-        <Formik
-            
-          onSubmit={(value) => {
-            if (localStorage.getItem('token') != null) {
-              decoded = jwt_decode(localStorage.getItem('token'));
+            reader.onloadend = () => {
+              file = image;
             }
-            admin_emailId = decoded.email;
-            console.log(file);
-            var axios = require('axios');
-            var data = new FormData();
-            console.log(signature);
-            console.log(admin_emailId);
-            console.log(decoded.email);
-            data.append('signature', signature);
-            data.append('file', file);
-            data.append('template_url', 'uploads/2020-12-13T04-27-09.039ZWhite Geometric Design Participation Certificate (1)-1.png');
-            data.append('admin_email', admin_emailId);
 
-            var config = {
-              method: 'post',
-              url: 'http://localhost:5000/generateCertificates/create',
-              data: data
-            };
+            reader.readAsDataURL(image)
+          }
 
-            axios(config)
-              .then(function (response) {
-                console.log(JSON.stringify(response.data));
-              })
-              .catch(function (error) {
-                console.log(error);
-              });
-          }}
+           _handleTemplateChange(e) {
+            e.preventDefault();
+
+            let reader = new FileReader();
+            let image = e.target.files[0];
+
+            reader.onloadend = () => {
+              template = image;
+            }
+
+            reader.readAsDataURL(image)
+          }
+            onPick(image){
+            this.setState({
+              image: {image},
+            })
+          }
+        
+  render(){
+    if(this.state.loaded==false)
+      {
+        return(
+        <Page>
+        <Container maxWidth="sm"></Container>
+        </Page>
+        )
+
+      }
+    else
+    {
+    return(
+          <Page
         >
-          {({
-            errors,
-            handleBlur,
-            handleChange,
-            handleSubmit,
-            isSubmitting,
-            touched,
-            values
-          }) => (
-            <Card className={classes.card}>
-              <CardContent>
-                <Typography variant="h2" component="h2">
-                  Generate Certificates
-                          </Typography>
-                <form onSubmit={handleSubmit}>
-                 
-                  <Box mt={3}>
+          <Container maxWidth="sm">
+            <Formik
+                
+              onSubmit={(value) => {
+                if (localStorage.getItem('token') != null) {
+                  decoded = jwt_decode(localStorage.getItem('token'));
+                }
+                admin_emailId = decoded.email;
+                console.log(file);
+                var axios = require('axios');
+                var data = new FormData();
+                console.log(this.state.signature);
+                console.log(admin_emailId);
+                console.log(decoded.email);
+                data.append('signature', this.state.signature);
+                data.append('file', file);
+                data.append('template_url', 'uploads/2020-12-13T04-27-09.039ZWhite Geometric Design Participation Certificate (1)-1.png');
+                data.append('admin_email', admin_emailId);
 
+                var config = {
+                  method: 'post',
+                  url: 'http://localhost:5000/generateCertificates/create',
+                  data: data
+                };
+
+                axios(config)
+                  .then(function (response) {
+                    console.log(JSON.stringify(response.data));
+                  })
+                  .catch(function (error) {
+                    console.log(error);
+                  });
+              }}
+
+            >
+              {({
+                errors,
+                handleBlur,
+                handleChange,
+                handleSubmit,
+                isSubmitting,
+                touched,
+                values
+              }) => (
+                <Card>
+                  <CardContent>
+                    <Typography variant="h2" component="h2">
+                      Generate Certificates
+                              </Typography>
+                    <form onSubmit={handleSubmit}>
                     
-
-                    <TextField
-                      label="signature"
-                      value={signature}
-                      id="outlined-margin-normal"
-                      name="signature"
-                      className={classes.textField}
-                      onChange={handlechange}
-                      helperText="Signature : Your Name "
-                      margin="normal"
-                      variant="outlined"
-                    />
-                  </Box>
-                  <div style={{ marginTop: 10 + "px", marginBottom: 10 + "px", marginLeft: 20 + "px" }}>
-                    <span style={{ fontSize: 15 + "px", fontFamily: "monospace" }}><b>Upload Your CSV File:-</b></span>
-                    <input
-                      accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
-                      type="file"
-                      style={{ marginLeft: 10 + "px" }}
-                      onChange={_handleImageChange}
-                      name="csvfile"
-                    />
-                  </div>
-                  <Box my={2}>
-                    <Button
-                      color="primary"
-                      disabled={isSubmitting}
-                      fullWidth
-                      size="large"
-                      type="submit"
-                      variant="contained"
-                    >
-                      Submit
-                  </Button>
-                  </Box>
-                 
-                  <br></br>
-                  <br></br>
-                </form>
-              </CardContent>
-            </Card>
-          )}
-        </Formik>
-      </Container>
-    </Page>
-  );
-};
-
-export default ProductList;
+                      <Box mt={3}>
+                        
+                      <ImagePicker 
+                            images={this.state.imageList.map((image, i) => ({src: image, value: i}))}
+                            onPick={this.onPick}
+                          />
+                        <TextField
+                          label="signature"
+                          value={this.state.signature}
+                          id="outlined-margin-normal"
+                          name="signature"
+                          onChange={this.handlechange}
+                          helperText="Signature : Your Name "
+                          margin="normal"
+                          variant="outlined"
+                        />
+                      </Box>
+                      <div style={{ marginTop: 10 + "px", marginBottom: 10 + "px", marginLeft: 20 + "px" }}>
+                        <span style={{ fontSize: 15 + "px", fontFamily: "monospace" }}><b>Upload Your CSV File:-</b></span>
+                        <input
+                          accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel"
+                          type="file"
+                          style={{ marginLeft: 10 + "px" }}
+                          onChange={this._handleImageChange}
+                          name="csvfile"
+                        />
+                      </div>
+                      <Box my={2}>
+                        <Button
+                          color="primary"
+                          disabled={isSubmitting}
+                          fullWidth
+                          size="large"
+                          type="submit"
+                          variant="contained"
+                        >
+                          Submit
+                      </Button>
+                      </Box>
+                    
+                      <br></br>
+                      <br></br>
+                    </form>
+                  </CardContent>
+                </Card>
+              )}
+            </Formik>
+          </Container>
+        </Page>
+    )
+  }
+ }
+}
