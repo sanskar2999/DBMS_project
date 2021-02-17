e0c432d573e0fb3333ad5133b971836eb2965319
sanskar <sanskar2999@gmail.com>
2021-02-18 00:11:47 +0530

    font fix

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import GlobalStyles from 'src/components/GlobalStyles';
 import 'src/mixins/chartjs';
 import theme from 'src/theme';
 import routes from 'src/routes';
+import "tailwindcss/dist/base.css";
 
 
 const App = () => {
